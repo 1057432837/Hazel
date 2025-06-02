418de19f992a8a26624210a16aa381ccd5ad2150
笑川 刘 <lxc.990131@gmail.com>
2025-06-02 08:05:54 +0800

    6.2 8:05

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -55,31 +55,30 @@ namespace Hazel {
 	class EventDispatcher
 	{
 		template<typename T>
+
 		using EventFn = std::function<bool(T&)>;
 
 	public:
-		EventDispatcher(Event& event) : m_Event(event){}
+		EventDispatcher(Event& event) : m_Event(event){ }
 
 		template<typename T>
+
 		bool Dispatch(EventFn<T> func) {
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
 				m_Event.Handled = func(*(T*)&m_Event);
 				return true;
+
 			}
 			return false;
+
 		}
 
 	private:
 		Event& m_Event;
-		//template<typename T>
-		//using EventFn = std::function<bool(T&)>;
 
 	};
 
-	inline std::ostream& operator<<(std::ostream& os, const Event& e) {
-		return os << e.ToString();
-
-	}
+	inline std::ostream& operator<<(std::ostream& os, const Event& e) { return os << e.ToString(); }
 
 }
