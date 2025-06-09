477ddc5e208b159cdacb4ba2adea14f8fe4a8bca
笑川 刘 <lxc.990131@gmail.com>
2025-06-09 09:45:56 +0800

    6.9 9:45

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -39,6 +39,9 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		static Application* s_Instance;
+
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+
 	};
 
 	Application* CreateApplication();
--- Hazel/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLContext.cpp
@@ -5,6 +5,7 @@
 #include "GL/GL.h"
 
 #include "Hazel/Core.h"
+#include "Hazel/Log.h"
 #include "OpenGLContext.h"
 
 namespace Hazel {
@@ -21,6 +22,11 @@ namespace Hazel {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		HZ_CORE_INFO("OpenGL Info:");
+		HZ_CORE_INFO("  Vender: {0}", glGetString(GL_VENDOR));
+		HZ_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		HZ_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 
 	}
 
