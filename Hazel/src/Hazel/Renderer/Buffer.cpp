d7ab3de0cd0f3b1e678f1b9dc9d10d5af35979bd
笑川 刘 <lxc.990131@gmail.com>
2025-06-13 20:18:13 +0800

    6.13 20:18

--- Hazel/src/Hazel/Renderer/Buffer.cpp
+++ Hazel/src/Hazel/Renderer/Buffer.cpp
@@ -6,6 +6,53 @@
 #include "Platform/OpenGL/OpenGLBuffer.h"
 
 namespace Hazel {
+	BufferLayout::BufferLayout() {
+	
+	}
+
+	BufferLayout::BufferLayout(const std::initializer_list<BufferElement>& elements) : m_Elements(elements) {
+		CalculateOffsetAndStride();
+
+	}
+
+	std::vector<BufferElement>::iterator BufferLayout::begin() {
+		return m_Elements.begin();
+	
+	}
+
+	std::vector<BufferElement>::iterator BufferLayout::end() {
+		return m_Elements.end();
+
+	}
+
+	std::vector<BufferElement>::const_iterator BufferLayout::begin() const {
+		return m_Elements.begin();
+	
+	}
+
+	std::vector<BufferElement>::const_iterator BufferLayout::end() const {
+		return m_Elements.end();
+	
+	}
+
+	void BufferLayout::CalculateOffsetAndStride() {
+		uint32_t offset = 0;
+		m_Stride = 0;
+
+		for (auto& element : m_Elements)
+		{
+			element.Offset = offset;
+			offset += element.Size;
+			m_Stride += element.Size;
+
+		}
+
+	}
+
+	VertexBuffer::~VertexBuffer() {
+	
+	}
+
 	VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size) {
 		switch (Renderer::GetAPI())
 		{
@@ -27,6 +74,10 @@ namespace Hazel {
 
 	}
 
+	IndexBuffer::~IndexBuffer() {
+	
+	}
+
 	IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t size) {
 		switch (Renderer::GetAPI())
 		{
@@ -45,6 +96,7 @@ namespace Hazel {
 
 		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
+
 	}
 
 }
--- Hazel/src/Hazel/Renderer/Buffer.h
+++ Hazel/src/Hazel/Renderer/Buffer.h
@@ -156,50 +156,35 @@ namespace Hazel {
 	class BufferLayout
 	{
 	public:
-		BufferLayout() { }
+		BufferLayout();
 
-		BufferLayout(const std::initializer_list<BufferElement>& elements) : m_Elements(elements) {
-			CalculateOffsetAndStride();
-
-		}
+		BufferLayout(const std::initializer_list<BufferElement>& elements);
 
 		inline uint32_t GetStride() const { return m_Stride; }
 
 		inline const std::vector<BufferElement>& GetElements() const { return m_Elements; }
 
-		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
+		std::vector<BufferElement>::iterator begin();
 
-		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
+		std::vector<BufferElement>::iterator end();
 
-		std::vector<BufferElement>::const_iterator begin() const { return m_Elements.begin(); }
+		std::vector<BufferElement>::const_iterator begin() const;
 
-		std::vector<BufferElement>::const_iterator end() const { return m_Elements.end(); }
+		std::vector<BufferElement>::const_iterator end() const;
 
 	private:
 		std::vector<BufferElement> m_Elements;
 
 		uint32_t m_Stride = 0;
 
-		void CalculateOffsetAndStride() {
-			uint32_t offset = 0;
-			m_Stride = 0;
-
-			for (auto& element : m_Elements)
-			{
-				element.Offset = offset;
-				offset += element.Size;
-				m_Stride += element.Size;
-
-			}
-
-		}
+		void CalculateOffsetAndStride();
 
 	};
 
 	class VertexBuffer
 	{
 	public:
-		virtual ~VertexBuffer() { };
+		virtual ~VertexBuffer();
 
 		virtual void Bind() const = 0;
 
@@ -218,7 +203,7 @@ namespace Hazel {
 	class IndexBuffer
 	{
 	public:
-		virtual ~IndexBuffer() { };
+		virtual ~IndexBuffer();
 
 		virtual void Bind() const = 0;
 
