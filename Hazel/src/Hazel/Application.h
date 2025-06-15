176ece630f61777de5f8d72a3d4b0a3b04ebfd78
笑川 刘 <lxc.990131@gmail.com>
2025-06-15 18:55:05 +0800

    6.15 18:55

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -8,6 +8,7 @@
 #include "Hazel/ImGui/ImGuiLayer.h"	
 #include "Hazel/Renderer/Shader.h"
 #include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/VertexArray.h"
 
 namespace Hazel {
 	class HAZEL_API Application
@@ -42,13 +43,13 @@ namespace Hazel {
 
 		static Application* s_Instance;
 
-		unsigned int m_VertexArray;
+		std::shared_ptr<Shader> m_Shader;
 
-		std::unique_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
 
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::shared_ptr<VertexBuffer> m_VertexBuffer;
 
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<IndexBuffer> m_IndexBuffer;
 
 	};
 
