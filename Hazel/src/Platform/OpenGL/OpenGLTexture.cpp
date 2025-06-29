b3b0467a4c36d1860b0f05f5c1a7dbef15e61b50
笑川 刘 <lxc.990131@gmail.com>
2025-06-29 19:27:32 +0800

    6.29 19:27

--- Hazel/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -8,6 +8,7 @@
 namespace Hazel {
 	OpenGLTexture2D::OpenGLTexture2D(const std::string& path) : m_Path(path) {
 		int width, height, channels;
+		stbi_set_flip_vertically_on_load(1);
 		stbi_uc* data = stbi_load(path.c_str(), &width, &height, &channels, 0);
 		HZ_CORE_ASSERT(data, "Failed to load image!");
 		m_Width = width;
@@ -17,7 +18,7 @@ namespace Hazel {
 		glTextureStorage2D(m_RendererID, 1, GL_RGB8, m_Width, m_Height);
 
 		glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 		glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, GL_RGB, GL_UNSIGNED_BYTE, data);
 
