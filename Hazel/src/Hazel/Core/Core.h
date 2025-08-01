ecacf3e22e1063b9fe5a78341a821d2e2d56287a
笑川 刘 <lxc.990131@gmail.com>
2025-08-01 18:57:11 +0800

    8.1 18:57

--- Hazel/src/Hazel/Core/Core.h
+++ Hazel/src/Hazel/Core/Core.h
@@ -2,19 +2,63 @@
 
 #include "memory"
 
-#ifdef HZ_PLATFORM_WINDOWS
-	#if HZ_DYNAMIC_LINK
-		#ifdef HZ_BUILD_DLL
-			#define HAZEL_API __declspec(dllexport)
-		#else
-			#define HAZEL_API __declspec(dllimport)
-		#endif
-	#else
-		#define HAZEL_API
-#endif
-#else
-	#error Hazel only supports Windows!
-#endif
+/*----------------------Windows----------------------*/
+#ifdef _WIN32										//
+	#ifdef _WIN64									//
+		#define HZ_PLATFORM_WINDOWS					//
+	#else											//
+		#error "x86 Builds are not supported!"		//
+	#endif											//
+													//
+/*--------------------Apple & Mac--------------------*/
+#elif defined(__APPLE__) || defined(__MAC__)		//
+	#include <TargetConditionals.h>					//
+	#if TARGET_IPHONE_SIMULATOR == 1				//
+		#error "IOS simulator is not supported!"	//
+	#elif TARGET_OS_IPHONE == 1						//
+		#define HZ_PLATFORM_IOS						//
+		#error "IOS is not supported!"				//
+	#elif TARGET_OS_MAC == 1						//
+		#define HZ_PLATFORM_MACOS					//
+		#error "MacOS is not supported!"			//
+	#else											//
+		#error "Unknown Apple platform!"			//
+	#endif											//
+													//
+/*----------------------Android----------------------*/
+#elif defined(__ANDROID__)							//
+	#define HZ_PLATFORM_ANDROID						//
+	#error "Android is not supported!"				//
+													//
+/*-----------------------Linux-----------------------*/
+#elif defined(__LINUX__)							//
+	#define HZ_PLATFORM_LINUX						//
+	#error "Linux is not supported!"				//
+													//
+/*----------------------Unknown----------------------*/
+#else												//
+	#error "Unknown platform!"						//
+													//
+#endif												//
+													//
+/*---------------------------------------------------*/
+
+/*----------------Windows DLL support----------------*/
+#ifdef HZ_PLATFORM_WINDOWS							//
+	#if HZ_DYNAMIC_LINK								//
+		#ifdef HZ_BUILD_DLL							//
+			#define HAZEL_API __declspec(dllexport)	//
+		#else										//
+			#define HAZEL_API __declspec(dllimport)	//
+		#endif										//
+	#else											//
+		#define HAZEL_API							//
+#endif												//
+#else												//
+	#error Hazel only supports Windows!				//
+#endif												//
+													//
+/*---------------------------------------------------*/
 
 #ifdef HZ_ENABLE_ASSERTS
 	#define HZ_ASSERT(x, ...) { if(!(x)) {HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); }}
