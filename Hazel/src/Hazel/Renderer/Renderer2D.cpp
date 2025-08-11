027447fe8712dab9366027a547d0df6474c58d4c
笑川 刘 <lxc.990131@gmail.com>
2025-08-11 09:09:28 +0800

    8.11 9:09

--- Hazel/src/Hazel/Renderer/Renderer2D.cpp
+++ Hazel/src/Hazel/Renderer/Renderer2D.cpp
@@ -1,5 +1,7 @@
 #include "hzpch.h"
 
+#include "glm/gtc/matrix_transform.hpp"
+
 #include "Renderer2D.h"
 #include "VertexArray.h"
 #include "Shader.h"
@@ -64,7 +66,6 @@ namespace Hazel {
 	void Renderer2D::BeginScene(const OrthographicCamera& camera) {
 		s_Data->FlatColorShader->Bind();
 		s_Data->FlatColorShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
-		s_Data->FlatColorShader->SetMat4("u_Transform", glm::mat4(1.0f));
 
 	}
 
@@ -80,6 +81,9 @@ namespace Hazel {
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color) {
 		s_Data->FlatColorShader->Bind();
 		s_Data->FlatColorShader->SetFloat4("u_Color", color);
+
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+		s_Data->FlatColorShader->SetMat4("u_Transform", transform);
 
 		s_Data->QuadVertexArray->Bind();
 		RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
