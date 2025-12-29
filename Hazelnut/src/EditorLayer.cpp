8f7866c02c1237e9fc64f66b30d5ffc05f911928
lxc <lxc.990131@gmail.com>
2025-12-29 19:44:55 +0800

    12.29 19:44

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -32,10 +32,10 @@ namespace Hazel {
 		auto redSquare = m_ActiveScene->CreateEntity("Red Square");
 		redSquare->AddComponent<SpriteRendererComponent>(glm::vec4{ 1.0f, 0.0f, 0.0f, 1.0f });
 
-		m_CameraEntity = m_ActiveScene->CreateEntity("Camera Entity");
+		m_CameraEntity = m_ActiveScene->CreateEntity("Camera A");
 		m_CameraEntity->AddComponent<CameraComponent>();
 
-		m_SecondCamera = m_ActiveScene->CreateEntity("Clip-Space Entity");
+		m_SecondCamera = m_ActiveScene->CreateEntity("Camera B");
 		m_SecondCamera->AddComponent<CameraComponent>().Primary = false;
 
 		class CameraController : public ScriptableEntity
@@ -212,7 +212,7 @@ namespace Hazel {
 
 		m_SceneHierarchyPanel->OnImGuiRender();
 
-		ImGui::Begin("Settings");
+		ImGui::Begin("Stats");
 
 		auto stats = Renderer2D::GetStats();
 		ImGui::Text("Renderer2D Stats: ");
@@ -220,40 +220,6 @@ namespace Hazel {
 		ImGui::Text("Quads: %d", stats.QuadCount);
 		ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
 		ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
-
-		if (m_SquareEntity)
-		{
-			ImGui::Separator();
-
-			auto& tag = m_SquareEntity->GetComponent<TagComponent>().Tag;
-			ImGui::Text("%s", tag.c_str());
-
-			auto& m_SquareColor = m_SquareEntity->GetComponent<SpriteRendererComponent>().Color;
-			ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
-
-			ImGui::Separator();
-
-		}
-		
-		ImGui::DragFloat3("Camera Transform", glm::value_ptr(m_CameraEntity->GetComponent<TransformComponent>().Transform[3]));
-
-		if (ImGui::Checkbox("Camera A", &m_PrimaryCamera))
-		{
-			m_CameraEntity->GetComponent<CameraComponent>().Primary = m_PrimaryCamera;
-			m_SecondCamera->GetComponent<CameraComponent>().Primary = !m_PrimaryCamera;
-		
-		}
-
-		{
-			auto& camera = m_SecondCamera->GetComponent<CameraComponent>().Camera;
-			float orthoSize = camera.GetOrthographicSize();
-			if (ImGui::DragFloat("Second Camera Ortho Size", &orthoSize))
-			{
-				camera.SetOrthographicSize(orthoSize);
-
-			}
-			
-		}
 
 		ImGui::End();
 
--- Hazelnut/src/Panels/SceneHierarchyPanel.cpp
+++ Hazelnut/src/Panels/SceneHierarchyPanel.cpp
@@ -192,6 +192,19 @@ namespace Hazel {
 
 		}
 
+		if (entity.HasComponent<SpriteRendererComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(SpriteRendererComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Sprite Renderer"))
+			{
+				auto& src = entity.GetComponent<SpriteRendererComponent>();
+				ImGui::ColorEdit4("Color", glm::value_ptr(src.Color));
+
+				ImGui::TreePop();
+
+			}
+
+		}
+
 	}
 
 	Ref<SceneHierarchyPanel> SceneHierarchyPanel::Create() {
