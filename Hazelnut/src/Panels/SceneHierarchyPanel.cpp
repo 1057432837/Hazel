b6c7e505eaabc64cd162af46e47fc35514b69c71
lxc <lxc.990131@gmail.com>
2025-12-30 20:35:07 +0800

    12.30 20:35

--- Hazelnut/src/Panels/SceneHierarchyPanel.cpp
+++ Hazelnut/src/Panels/SceneHierarchyPanel.cpp
@@ -90,33 +90,45 @@ namespace Hazel {
 		float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
 		ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
 
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.9f, 0.2f, 0.2f, 1.0f });
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
 		if (ImGui::Button("x", buttonSize))
 		{
 			values.x = resetValue;
 
 		}
+		ImGui::PopStyleColor(3);
 
 		ImGui::SameLine();
 		ImGui::DragFloat("##x", &values.x, 0.1f);
 		ImGui::PopItemWidth();
 		ImGui::SameLine();
 
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.3f, 0.8f, 0.3f, 1.0f });
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
 		if (ImGui::Button("y", buttonSize))
 		{
 			values.y = resetValue;
 
 		}
+		ImGui::PopStyleColor(3);
 
 		ImGui::SameLine();
 		ImGui::DragFloat("##y", &values.y, 0.1f);
 		ImGui::PopItemWidth();
 		ImGui::SameLine();
 
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.2f, 0.35f, 0.9f, 1.0f });
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
 		if (ImGui::Button("z", buttonSize))
 		{
 			values.z = resetValue;
 
 		}
+		ImGui::PopStyleColor(3);
 
 		ImGui::SameLine();
 		ImGui::DragFloat("##z", &values.z, 0.1f);
