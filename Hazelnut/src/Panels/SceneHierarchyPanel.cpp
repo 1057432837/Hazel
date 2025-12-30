dcccdd8ef0ec00ce8ff709e3b9b429a392c07716
lxc <lxc.990131@gmail.com>
2025-12-30 20:26:10 +0800

    12.30 20:26

--- Hazelnut/src/Panels/SceneHierarchyPanel.cpp
+++ Hazelnut/src/Panels/SceneHierarchyPanel.cpp
@@ -1,4 +1,5 @@
 #include "imgui/imgui.h"
+#include "imgui/imgui_internal.h"
 #include "glm/gtc/type_ptr.hpp"
 
 #include "SceneHierarchyPanel.h"
@@ -77,6 +78,56 @@ namespace Hazel {
 
 	}
 
+	static void DrawVec3Control(const std::string& label, glm::vec3& values, float resetValue = 0.0f, float columnWidth = 100.0f) {
+		ImGui::Columns(2);
+		ImGui::SetColumnWidth(0, columnWidth);
+		ImGui::Text(label.c_str());
+		ImGui::NextColumn();
+
+		ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
+
+		float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+		ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
+
+		if (ImGui::Button("x", buttonSize))
+		{
+			values.x = resetValue;
+
+		}
+
+		ImGui::SameLine();
+		ImGui::DragFloat("##x", &values.x, 0.1f);
+		ImGui::PopItemWidth();
+		ImGui::SameLine();
+
+		if (ImGui::Button("y", buttonSize))
+		{
+			values.y = resetValue;
+
+		}
+
+		ImGui::SameLine();
+		ImGui::DragFloat("##y", &values.y, 0.1f);
+		ImGui::PopItemWidth();
+		ImGui::SameLine();
+
+		if (ImGui::Button("z", buttonSize))
+		{
+			values.z = resetValue;
+
+		}
+
+		ImGui::SameLine();
+		ImGui::DragFloat("##z", &values.z, 0.1f);
+		ImGui::PopItemWidth();
+
+		ImGui::PopStyleVar();
+
+		ImGui::Columns(1);
+
+	}
+	
 	void SceneHierarchyPanel::DrawComponents(Entity entity) {
 		if (entity.HasComponent<TagComponent>())
 		{
@@ -98,7 +149,7 @@ namespace Hazel {
 			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
 			{
 				auto& tc = entity.GetComponent<TransformComponent>();
-				ImGui::DragFloat3("Position", glm::value_ptr(tc.Translation), 0.1f);
+				DrawVec3Control("Translation", tc.Translation);
 
 				ImGui::TreePop();
 
